82a26a73d38048b432a10494463dbeb2c8363256
Tiago Andrade <tailergamer7@gmail.com>
2021-02-24 12:50:29 -0400

    :sparkles: feat: Third class

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -24,8 +24,9 @@ class UserController {
 
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
-export { UserController }
+export { UserController };
+
