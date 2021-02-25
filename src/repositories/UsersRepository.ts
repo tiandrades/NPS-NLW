ba60020d07645d5a5f41a92fe9bd0d1472a7de7b
Tiago Andrade <tailergamer7@gmail.com>
2021-02-25 13:14:20 -0400

    feat: SendMail Controller and Service and using handlebars for email html

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -2,6 +2,6 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../models/User';
 
 @EntityRepository(User)
-class UsersRepository extends Repository<User> {}
+class UsersRepository extends Repository<User> {};
 
-export { UsersRepository }
+export { UsersRepository };
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 
+const sendMailController = new SendMailController();
+
 router.post('/users', userController.create);
 
-router.post('/surveys', surveysController.create)
-router.get('/surveys', surveysController.show)
+router.post('/surveys', surveysController.create);
+router.get('/surveys', surveysController.show);
+
+router.post('/sendMail', sendMailController.execute);
 
 export { router };
